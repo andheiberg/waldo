5a30af287a6693105e06a7289e1331aa82d1f51a
Andreas Heiberg <andreas@heiberg.io>
2018-01-02 21:25:23 +0000

    add composer packages
    Signed-off-by: Andreas Heiberg <andreas@heiberg.io>

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Andheiberg\Theme\ThemeServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Form' => Collective\Html\FormFacade::class,
     ],
 
 ];
